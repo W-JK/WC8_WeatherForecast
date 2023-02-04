e7dacabaccf7b51342c6403a3a44daa8078df80e
Wojtek <118207811+W-JK@users.noreply.github.com>
2023-02-04 15:38:12 +0000

    temperature for main added

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,7 +1,10 @@
 var APIKey = "a02d36afb369ec740ca0a2c969837b0a" // Key created for this task only 
-// var mainQueryURL = "https://api.openweathermap.org/data/2.5/weather"+ "q=London,London&appid=" + APIKey;  /* &exclude={part} wtaw po london to exclude data */
-var mainQueryURL = "https://api.openweathermap.org/data/2.5/forecast?id=524901&appid="+APIKey+"&units=metric";
+ var mainQueryURL = "https://api.openweathermap.org/data/2.5/weather?"+ "q=London,London&appid=" + APIKey+ "&units=metric";  /* &exclude={part} wtaw po london to exclude data */
+// var mainQueryURL = "https://api.openweathermap.org/data/2.5/forecast?id=524901&appid="+APIKey+"&units=metric";
+// var cityName = 
 
+
+var todayDate = moment().format('LLLL');
 
 $.ajax({
     url: mainQueryURL,
@@ -11,11 +14,20 @@ $.ajax({
 .then(function(weatherForecast) {
 
     console.log(mainQueryURL);
-
     console.log(weatherForecast);
 
+      
+    $(".date").html("<span>"+ todayDate + "</span>");
+    $(".city").html("<h1>"+ weatherForecast.name + "</h1>");
 
- $(".city").html("<h1>"+ weatherForecast.city.name + "</h1>");
+     //$(".weather-icon").html("<svg>"+ weatherForecast.data+ "</svg>");
+
+
+
+   
+    $(".temperature").html("<h1>"+ "Temperature: " + weatherForecast.main.temp + " 'C" + "</h1>");
+    $(".humidity").html("<div>"  +  weatherForecast.main.humidity +  "</div>")
+    
 
 
 });
