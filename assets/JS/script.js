0976a1a402b60922536899d982429fd7a6d5ad8f
Wojtek <118207811+W-JK@users.noreply.github.com>
2023-02-04 13:51:53 +0000

    current weather - skeleton

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,16 +1,21 @@
 var APIKey = "a02d36afb369ec740ca0a2c969837b0a" // Key created for this task only 
-var mainQueryURL = "https://api.openweathermap.org/data/2.5/weather"+ "q=London,London" +"&appid=" + APIKey;  /* &exclude={part} wtaw po london to exclude data */
+// var mainQueryURL = "https://api.openweathermap.org/data/2.5/weather"+ "q=London,London&appid=" + APIKey;  /* &exclude={part} wtaw po london to exclude data */
+var mainQueryURL = "https://api.openweathermap.org/data/2.5/forecast?id=524901&appid="+APIKey+"&units=metric";
+
 
 $.ajax({
     url: mainQueryURL,
     metod:"GET"
 })
 
-.then(function(response) {
+.then(function(weatherForecast) {
 
     console.log(mainQueryURL);
 
-    console.log(response);
+    console.log(weatherForecast);
+
+
+ $(".city").html("<h1>"+ weatherForecast.city.name + "</h1>");
 
 
 });
