36e593b5e046febc2669eba50035755d64eb2dd2
Wojtek <118207811+W-JK@users.noreply.github.com>
2023-02-05 12:54:47 +0000

    daily weather card skeleton

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,24 +1,30 @@
  var APIKey = "a02d36afb369ec740ca0a2c969837b0a" // Key created for this task only 
  var mainQueryURL = "https://api.openweathermap.org/data/2.5/weather?"+ "q=London,London&appid=" + APIKey+ "&units=metric";  /* &exclude={part} wtaw po london to exclude data */
-// var mainQueryURL = "https://api.openweathermap.org/data/2.5/forecast?id=524901&appid="+APIKey+"&units=metric";
- var cityNames = ["London","Edinburgh"]
 
+ var cityNames = ["London","Edinburgh"]
 
 var todayDate = moment().format('LLLL');
 
+
+
+
 $.ajax({
-    url: mainQueryURL,
-    metod:"GET"
+    url: mainQueryURL,    
+    metod:"GET",   
+    
+
 })
 
 .then(function(weatherForecast) {
 
     console.log(mainQueryURL);
+    
     console.log(weatherForecast);
 
     // ------------------- current day sectoion ------------- //
    
     $(".date").html("<span>"+ todayDate + "</span>");
+    console.log(todayDate)
     $(".city").html("<h1>"+ weatherForecast.name + "</h1>");
 
      // ------------- dynamic weather icon ------------------ //
@@ -35,12 +41,71 @@ $.ajax({
     
     // ------------------ current day section end -------------- // 
 
+    $(".sub-container").each(function(){
+            
+            
+        var futureDate = moment().add(1,'d').format('LLLL')
+        console.log(futureDate)
+        console.table(futureDate)
+        // console.log(futureDate._d) test point if not formated 
+
+        $(".future-date").html("<div>"  + futureDate + "</div>") //  futureDate._d if futureDate not formated
+    });
+
+      
+
+});
+
+
 
 
     // ------------------- 5 day forecast sectoion ------------- //
 
+        var citylat = weatherForecast.coord.lat ;
+        var citylon = weatherForecast.coord.lon;
 
 
-});
+        var dailyQueryURL = "api.openweathermap.org/data/2.5/forecast?lat=" + citylat +"&lon=" + citylon + "&appid=" + APIKey + "&units=metric"; 
+        console.log(dailyQueryURL)
+
+        $.ajax({
+            url: dailyQueryURL,    
+            metod:"GET",        
+            
+        
+        })
+
+        .then(function(dailyForecast) {
+
+            console.log(mainQueryURL);
+            
+            console.log(dailyForecast); 
+
+
+
+       
+
+    });
+
+// -------------------------------  5day forecast end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
