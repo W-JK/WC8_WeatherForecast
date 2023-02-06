05f92e8d7d12e25b68b2563ab11054370d953663
Wojtek <118207811+W-JK@users.noreply.github.com>
2023-02-06 01:55:19 +0000

    progress save + delate button added + main URL live update function

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,27 +1,57 @@
  
  var APIKey = "a02d36afb369ec740ca0a2c969837b0a" // Key created for this task only 
- 
- var mainQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=London,London&appid=" + APIKey+ "&units=metric" ;  /* &exclude={part} wtaw po london to exclude data */
-
  var cityNames = ["London","Edinburgh"]
-
  var todayDate = moment().format('LLLL');
+ 
+ 
 
+$("#search-button").on("click",function(event){
+     event.preventDefault();
+
+     
+
+    var city = $("#search-input").val().trim(); 
+    if ($("#search-input").val() == '' ) {      
+        alert("Please type city name")
+        var cityURL= "London"
+    
+        var mainQueryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ cityURL + "&appid=" + APIKey+ "&units=metric" ;  /* &exclude={part} wtaw po london to exclude data */
+     
+    }
+    
+           
+        
+    else {       
+
+        cityNames.push(city);
+        console.log(city)
+        console.table(cityNames)
+        var mainQueryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ city + "&appid=" + APIKey+ "&units=metric" ;
+        localStorage.setItem(city, mainQueryURL );   
+        renderButtons(); 
+    } 
+
+     
+
+ 
+    
+
+
+
+ 
  $.ajax({
     url: mainQueryURL,    
     metod:"GET"   
     
 
-})
-
-.then(function(weatherForecast) {
+}).then(function(weatherForecast) {
     
 
     console.log(mainQueryURL);
     
     console.log(weatherForecast);
 
-    // ------------------- current day sectoion ------------- //
+    // ------------------- current day section ------------- //
    
     $(".date").html("<span>"+ todayDate + "</span>");
     console.log(todayDate)
@@ -43,7 +73,7 @@
 
     
 
-        for (let day = 1; ; day++) {          
+        for (let day = 1; ; day++) {                 
             
         var futureDate = moment().add(day,'d').format('LLLL') 
         // console.log(futureDate)     
@@ -53,55 +83,86 @@
                 return;
             
             }
+            
              $(".future-date").html("<div>"  + futureDate + "</div>") //  futureDate._d if futureDate not formated
         };
+
+        
         
         // console.log(futureDate._d) test point if not formated       
 
 }); /* ------- main query brackets */
 
-
+});
 // --------------------- Function for displaying city buttons ----------------------------------------------
 
         // ----------- render buttons --------------------------
 
         function renderButtons() {
-            $("#history").empty();
+             $("#history").empty();
             for (var i=0; i< cityNames.length; i++){
                 var a = $("<li>");
                 // a.addType("button");
                 a.addClass("btn btn-info");
-                a.attr("data-name",cityNames[i]);
+                a.attr("data-city",cityNames[i]);
                 a.text(cityNames[i]);
                 $("#history").append(a) // adding buttons to search history
 
             }
-        } // --------   render buttons end
+        } 
+        
+        
+        // Delete button function 
+
+        function deleteSearchItems() {            
+            localStorage.clear(); //clear local storage 
+            $("#history").empty(); // remove city buttons
+            location.reload();    // reload/refresh page         
+        }
+
+        renderButtons(); // --------   render buttons end
+
+            
+             
+
+          
+          
+        
+       
+        
 
 
+
+
+
+
+/*   before test 
         $("#search-button").on("click",function(event){
             event.preventDefault();
 
             var city = $("#search-input").val().trim();
             cityNames.push(city);
+            console.log(city)
             console.table(cityNames)
-            
+            var mainQueryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ city + "&appid=" + APIKey+ "&units=metric" ;
+            localStorage.setItem(city, mainQueryURL );        
+                  
             renderButtons();
+           
+           
 
-        });
+        });    
 
+// test --- to be removed 
 
-        $("#search-button").on("click",".city",weatherForecast);
-        renderButtons();
-
-
-
-
-
-
-
-
-
+function populateStorage() {
+    var mainQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=a02d36afb369ec740ca0a2c969837b0a&units=metric"
+    localStorage.setItem(London, mainQueryURL ); 
+    //var mainQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=a02d36afb369ec740ca0a2c969837b0a&units=metric"
+    //localStorage.setItem(Edinburgh, "https://api.openweathermap.org/data/2.5/weather?q=London&appid=a02d36afb369ec740ca0a2c969837b0a&units=metric"  ); 
+    
+}
+// --- test end */
 
 
 
