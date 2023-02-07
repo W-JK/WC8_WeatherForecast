0632b8e1fa93606d66568a1ef5e52803088cffad
Wojtek <118207811+W-JK@users.noreply.github.com>
2023-02-07 01:34:22 +0000

    Daily progres save +  5 day forecast added

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -33,8 +33,7 @@ $("#search-button").on("click",function(event){
 
      
 
- 
-    
+
 
 
 
@@ -56,6 +55,7 @@ $("#search-button").on("click",function(event){
     $(".date").html("<span>"+ todayDate + "</span>");
     console.log(todayDate)
     $(".city").html("<h1>"+ weatherForecast.name + "</h1>");
+    
 
      // ------------- dynamic weather icon ------------------ //
     var iconID = weatherForecast.weather[0].icon;
@@ -71,29 +71,127 @@ $("#search-button").on("click",function(event){
     
     // ------------------ current day section end -------------- // 
 
+    // --------------------- 5 day test section ---------------------------
+   
     
 
-        for (let day = 1; ; day++) {                 
+         for (let day = 1; day<5  ;day++) {                 
             
-        var futureDate = moment().add(day,'d').format('LLLL') 
-        // console.log(futureDate)     
+        var futureDate =   [ moment().add(day,'d').format('LLLL') ]
+         console.log(futureDate)           
+         // $(".future-date").html("<div>"  + futureDate + "</div>") //  futureDate._d if futureDate not formated      
+
+            }; 
+        // console.log(futureDate._d) test point if not formated  
+         
+        var lat = weatherForecast.coord.lat;
+        var lon = weatherForecast.coord.lon;
+        var secondaryQueryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&exclude=hourly&appid=" + APIKey+ "&units=metric&dt&exclude=daily";
+        console.log(secondaryQueryURL)
 
         
-                if (day === 5){
-                return;
+        $.ajax({
+            url: secondaryQueryURL,    
+            metod:"GET"   
             
-            }
-            
-             $(".future-date").html("<div>"  + futureDate + "</div>") //  futureDate._d if futureDate not formated
-        };
+        
+        }).then(function(weatherForecast) {
+            console.log(weatherForecast);
 
+            
+
+            $(".future-date1").html("<div>"+ weatherForecast.list[3].dt_txt +"</div>");//main.temp
+            var ficonID = weatherForecast.list[3].weather[0].icon;
+            // test point:  console.log(ficonID)
+            ficonSrc = "http://openweathermap.org/img/wn/"+ ficonID +"@2x.png " //dynamic icon src
+             // test point:  console.log(ficonSrc)
+             $("img.daily-weather-icon1").attr("src", ficonSrc) // passing attribute of an image into html
+             $(".daily-temperature1").html("<h3>"+ "Temperature: " + weatherForecast.list[4].main.temp + " 'C" + "</h3>");
+             $(".daily-humidity1").html("<div>" + "Humidity: " +  weatherForecast.list[4].main.humidity + " % " +  "</div>")
+             $(".daily-wind1").html("<div>" + "Wind Speed: " + weatherForecast.list[4].wind.speed + " KPH " + "<div>")
+            
+             
+             $(".future-date2").html("<div>"+ weatherForecast.list[11].dt_txt +"</div>");//main.temp
+            var ficonID = weatherForecast.list[12].weather[0].icon;
+            // test point:  console.log(ficonID)
+            ficonSrc = "http://openweathermap.org/img/wn/"+ ficonID +"@2x.png " //dynamic icon src
+             // test point:  console.log(ficonSrc)
+             $("img.daily-weather-icon2").attr("src", ficonSrc) // passing attribute of an image into html
+             $(".daily-temperature2").html("<h3>"+ "Temperature: " + weatherForecast.list[12].main.temp + " 'C" + "</h3>");
+             $(".daily-humidity2").html("<div>" + "Humidity: " +  weatherForecast.list[12].main.humidity + " % " +  "</div>")
+             $(".daily-wind2").html("<div>" + "Wind Speed: " + weatherForecast.list[12].wind.speed + " KPH " + "<div>")
+            
+             $(".future-date3").html("<div>"+ weatherForecast.list[20].dt_txt +"</div>");//main.temp
+             var ficonID = weatherForecast.list[20].weather[0].icon;
+             // test point:  console.log(ficonID)
+             ficonSrc = "http://openweathermap.org/img/wn/"+ ficonID +"@2x.png " //dynamic icon src
+              // test point:  console.log(ficonSrc)
+              $("img.daily-weather-icon3").attr("src", ficonSrc) // passing attribute of an image into html
+              $(".daily-temperature3").html("<h3>"+ "Temperature: " + weatherForecast.list[20].main.temp + " 'C" + "</h3>");
+              $(".daily-humidity3").html("<div>" + "Humidity: " +  weatherForecast.list[20].main.humidity + " % " +  "</div>")
+              $(".daily-wind3").html("<div>" + "Wind Speed: " + weatherForecast.list[20].wind.speed + " KPH " + "<div>")
+            
+              $(".future-date4").html("<div>"+ weatherForecast.list[28].dt_txt +"</div>");//main.temp
+              var ficonID = weatherForecast.list[28].weather[0].icon;
+              // test point:  console.log(ficonID)
+              ficonSrc = "http://openweathermap.org/img/wn/"+ ficonID +"@2x.png " //dynamic icon src
+               // test point:  console.log(ficonSrc)
+               $("img.daily-weather-icon4").attr("src", ficonSrc) // passing attribute of an image into html
+               $(".daily-temperature4").html("<h3>"+ "Temperature: " + weatherForecast.list[28].main.temp + " 'C" + "</h3>");
+               $(".daily-humidity4").html("<div>" + "Humidity: " +  weatherForecast.list[28].main.humidity + " % " +  "</div>")
+               $(".daily-wind4").html("<div>" + "Wind Speed: " + weatherForecast.list[28].wind.speed + " KPH " + "<div>")
+            
+               $(".future-date5").html("<div>"+ weatherForecast.list[36].dt_txt +"</div>");//main.temp
+               var ficonID = weatherForecast.list[36].weather[0].icon;
+               // test point:  console.log(ficonID)
+               ficonSrc = "http://openweathermap.org/img/wn/"+ ficonID +"@2x.png " //dynamic icon src
+                // test point:  console.log(ficonSrc)
+                $("img.daily-weather-icon5").attr("src", ficonSrc) // passing attribute of an image into html
+                $(".daily-temperature5").html("<h3>"+ "Temperature: " + weatherForecast.list[36].main.temp + " 'C" + "</h3>");
+                $(".daily-humidity5").html("<div>" + "Humidity: " +  weatherForecast.list[36].main.humidity + " % " +  "</div>")
+                $(".daily-wind5").html("<div>" + "Wind Speed: " + weatherForecast.list[36].wind.speed + " KPH " + "<div>")
+            
+         
+              
+            
+
+        });
+
+     
+
+         
+       
         
-        
-        // console.log(futureDate._d) test point if not formated       
+       // -   5day test section ----------------------------------------------------------------------------
+
+      
+       
+      
+
+
+
+
+
+
+
 
 }); /* ------- main query brackets */
 
+
+
+
+
+
 });
+
+
+
+
+
+
+
+
+
 // --------------------- Function for displaying city buttons ----------------------------------------------
 
         // ----------- render buttons --------------------------
@@ -101,10 +199,11 @@ $("#search-button").on("click",function(event){
         function renderButtons() {
              $("#history").empty();
             for (var i=0; i< cityNames.length; i++){
-                var a = $("<li>");
-                // a.addType("button");
+                var a = $("<li>");                
+                
                 a.addClass("btn btn-info");
                 a.attr("data-city",cityNames[i]);
+                // a.attr('onclick','fetchWeather(event)')
                 a.text(cityNames[i]);
                 $("#history").append(a) // adding buttons to search history
 
@@ -135,25 +234,7 @@ $("#search-button").on("click",function(event){
 
 
 
-
-/*   before test 
-        $("#search-button").on("click",function(event){
-            event.preventDefault();
-
-            var city = $("#search-input").val().trim();
-            cityNames.push(city);
-            console.log(city)
-            console.table(cityNames)
-            var mainQueryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ city + "&appid=" + APIKey+ "&units=metric" ;
-            localStorage.setItem(city, mainQueryURL );        
-                  
-            renderButtons();
-           
-           
-
-        });    
-
-// test --- to be removed 
+/*
 
 function populateStorage() {
     var mainQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=a02d36afb369ec740ca0a2c969837b0a&units=metric"
